f6cabfe0dc93d892a755571dfa0fc279e6e52faf
RomanVakulenko <vakula_48@mail.ru>
2023-05-20 17:49:34 +0300

    v.1

--- Slider game/ViewController.swift
+++ Slider game/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var slider: UISlider!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var score: UILabel!
+
+    var number: Int = 0
+    var round: Int = 0
+    var points: Int = 0
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        checkNumber()
     }
 
+    @IBAction func checkNumber() {
+        if round == 0 {
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            score.text = String(points)
+            round = 1
+        } else {
+            let numSlider = Int(slider.value.rounded())
 
+            if numSlider == number {
+                points += 10
+            } else if numSlider >= number-3 && numSlider <= number+3 {
+                points += 5
+            } else {
+                points += 1
+            }
+
+            if round == 5 {
+                let alert = UIAlertController( title: "Игра окончена",
+                                               message: "Вы набрали \(points) очков", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Начать заново", style: .default, handler: nil))
+                present(alert, animated: true, completion: nil)
+                round = 1
+                points = 0
+            } else {
+                round += 1
+            }
+
+            number = Int.random(in: 1...50)
+            label.text = String(number)
+            score.text = String(points)
+        }
+
+    }
 }
 
